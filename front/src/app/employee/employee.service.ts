7de77d6005521435af27813f60c9b5c2cd792898
achraf bilal <bilal_achraf@emsi-edu.ma>
2022-03-06 23:17:38 +0100

    Angular front Employee model and service are ready

---
+++ front/src/app/employee/employee.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './employee';
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServerUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) {}
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/all`);
+  }
+  public addEmployees(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/add`, employee);
+  }
+  public editEmployees(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/update`, employee);
+  }
+  public gdeleteEmployees(emmployeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/delete/${emmployeeId}`);
+  }
+}
---
+++ front/src/app/employee/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  jobTitle: string;
+  imageUrl: string;
+  employeeCode: string;
+}
